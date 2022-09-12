7df79c3578c6ce7ddb8b1fb3093af18e715eac9a
Viktor <papenson622@gmail.com>
2022-09-12 19:02:07 +0300

    first task

---
+++ 1.js
@@ -0,0 +1,3 @@
+// comments
+/*As you write code, you should regularly add comments to clarify the function of parts of your code. Good commenting can help communicate the intent of your code—both for others and for your future self
+h*/
